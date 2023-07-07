bad4c23ff0dc0ddd021292d02891346e7d71776a
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-08 02:41:27 +0530

    new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,97 @@
+import { Fragment, useState } from 'react'
+
+let navItems = [
+  {
+    label: 'Home',
+    route: '/',
+  },
+  {
+    label: 'About',
+    route: '#about',
+  },
+  {
+    label: 'Tokenomics',
+    route: '#token',
+  },
+  {
+    label: 'Telegram',
+    route: 'https://t.me/APECOIN2point0',
+  },
+  {
+    label: 'Twitter',
+    route: 'https://twitter.com/APECOIN2point0',
+  },
+]
+
+export default function Navbar() {
+  let [open, setOpen] = useState(false)
+  const route = '/'
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 mx-auto bg-white bg-opacity-[0.0001] backdrop-filter backdrop-blur-2xl z-10 max-w-7xl">
+        <div className="font-orbitron flex items-center justify-between  py-3 lg:justify-start lg:space-x-10 animate-top-left px-5">
+          <img src="/logo.svg" className="" alt="" />
+          <div className=" flex items-center flex-grow space-x-4 lg:w-0">
+            <nav
+              as="nav"
+              className="hidden space-x-10 lg:flex justify-end  items-center flex-grow mr-5"
+            >
+              {navItems.map((nav, i) => {
+                return (
+                  <a
+                    key={i}
+                    href={nav.route}
+                    className={`text-medium font-normal hover:text-[#ddf840]
+                     
+                     ${
+                       route == nav.route
+                         ? ' text-[#c9ed39] '
+                         : 'text-[#ffffff]'
+                     }
+                  `}
+                  >
+                    <div className=" flex justify-center items-center">
+                      <img src={nav.icon} className="mr-2" alt="" />
+                      <span>{nav.label}</span>
+                    </div>
+                  </a>
+                )
+              })}
+            </nav>
+            <a
+              href="/"
+              className="lg:flex hidden  justify-self-end whitespace-nowrap rounded-md cursor-pointer border border-transparent transition-colors hover:border-white   py-2 font-bold  px-3 text-sm hover:text-white bg-[#E8BA4B] hover:bg-black "
+            >
+              <span>BUY APE 2.0</span>
+            </a>
+            <div className=" lg:hidden flex justify-end w-full pr-3">
+              <button
+                onClick={() => setOpen(!open)}
+                className=" inline-flex items-center justify-center  rounded-md  p-2"
+              >
+                <span className="sr-only">Open menu</span>
+                <img src={open ? '/close.png' : '/horzBar.svg'} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`bg-[#3D3D3D] shadow-2xl rounded-lg fixed top-24 right-2 z-50 transition-opacity duration-300 ${
+          open ? 'opacity-100' : 'opacity-0'
+        } lg:hidden block`}
+      >
+        <ul className="">
+          {navItems.map((navItem) => (
+            <a
+              href={navItem.route}
+              className="py-2 text-white w-52 block hover:bg-yellow-900 hover:bg-opacity-60 font-medium px-5"
+            >
+              {navItem.label}
+            </a>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="">
+      <div className="flex w-full justify-between items-center  pt-20  sm:py-7 mb-20">
+        <img src="/hero.svg" className="w-[50%]   h-auto" />
+        <div className="flex-[1] w-[40%] flex flex-col px-2 items-center max-w-xs text-white text-center">
+          <h2 className="text-lg font-pop mb-3">Welcome To</h2>
+          <h1 className="text-4xl sm:text-7xl font-bold font-pop mb-3 font-arch">
+            APE 2.0
+          </h1>
+          <p className="sm:text-center text-[0.75rem] sm:text-base  text-start px-4 mb-4 sm:mb-8">
+            Missed Pepe 2.0 , Shiba 2.0 ? Don't Miss Ape 2.0
+          </p>
+
+          <div className="flex w-full justify-center">
+            <button className="bg-[#E8BA4B] py-2 text-[0.5rem] sm:text-base font-bold sm:font-medium px-3 rounded-md text-black font-pop">
+              Buy Pinksale
+            </button>
+            <button className="ml-1 sm:ml-3 bg-[#fff] py-2 text-[0.5rem] sm:text-base font-bold sm:font-medium px-3 rounded-md text-black font-pop">
+              Audit
+            </button>
+          </div>
+          <div className="w-full flex justify-center mt-8 gap-x-3">
+            <a href="https://t.me/APECOIN2point0" target='_blank'>
+
+            <img src="/social-media/telegram-white.svg" className='cursor-pointer' />
+            </a>
+            <a href="https://twitter.com/APECOIN2point0" target='_blank'>
+
+            <img src="/social-media/twitter-white.svg" className='cursor-pointer' />
+            </a>
+          </div>
+          <div className="hidden sm:flex flex-col ">
+            <p className="text-[#E8BA4B] text-[0.8vw] sm:text-base  mt-3 mb-2">
+              Contract Address
+            </p>
+            <p className="break-words">
+              0xfb66321d7c674995dfcc2cb67a30bc978dc862ad
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex sm:hidden w-full justify-center items-center flex-col pb-20 mt-5">
+        <p className="text-[#E8BA4B] mt-3 mb-2 text-center">Contract Address</p>
+        <p className="text-center text-white break-words w-full">
+          0xfb66321d7c674995dfcc2cb67a30bc978dc862ad
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
